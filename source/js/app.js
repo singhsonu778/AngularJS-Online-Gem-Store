459b7fb26904dbe3490506d3e657587874fd9d78
Sonu Kumar Singh <singhsonu778@gmail.com>
2016-01-12 11:05:12 +0530

    Use ng-repeat directive to display multiple elements

--- source/js/app.js
+++ source/js/app.js
@@ -1,14 +1,28 @@
 (function() {
-	var gem = {
-		name : 'Azurite',
-		price : 2.95,
-        canPurchase: true,
-        soldOut: false
-	};
+	var gems = [
+		{
+			name : 'Azurite',
+			price : 2.95,
+			canPurchase: true,
+			soldOut: false
+		},
+		{
+			name : 'Stone',
+			price : 5.95,
+			canPurchase: false,
+			soldOut: false
+		},
+		{
+			name : 'Diamond',
+			price : 8.95,
+			canPurchase: true,
+			soldOut: false
+		}
+	];
 
 	var app = angular.module('gemStore', []);
 	
 	app.controller('StoreController', function() {
-		this.product = gem;
+		this.products = gems;
 	});
 })();
