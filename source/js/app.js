876d6aff42883e4df31f84b1026338c8518fd967
Sonu Kumar Singh <singhsonu778@gmail.com>
2016-01-14 00:46:37 +0530

    Add Review Controller to handle new review

--- source/js/app.js
+++ source/js/app.js
@@ -100,4 +100,13 @@
             this.current = val || 0;
         };
     });
+    
+
+    app.controller('ReviewController', function() {
+		this.review = {};	
+		this.addReview = function(product) {
+			product.reviews.push(this.review);
+			this.review = {};
+		};
+    });
 })();
