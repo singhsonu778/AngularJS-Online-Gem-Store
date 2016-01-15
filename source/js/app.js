412af9948427f21207fdf4d7e29fee6b11adfd12
Sonu Kumar Singh <singhsonu778@gmail.com>
2016-01-15 23:04:41 +0530

    Create separate module for directives

--- source/js/app.js
+++ source/js/app.js
@@ -78,7 +78,7 @@
         }]
     }];
 
-    var app = angular.module('gemStore', []);
+    var app = angular.module('gemStore', ['productsStore']);
 
     app.controller('StoreController', function () {
         this.products = gems;
@@ -92,56 +92,4 @@
 			this.review = {};
 		};
     });
-    
-    app.directive('productDescription', function() {
-    	return {
-    		restrict: 'E',
-    		templateUrl: 'templates/product-description.html'
-    	};
-    });
-    
-    app.directive('productReview', function() {
-    	return {
-    		restrict: 'E',
-    		templateUrl: 'templates/product-review.html'
-    	};
-    });
-    
-    app.directive("productSpecs", function() {
-        return {
-          restrict: 'A',
-          templateUrl: "templates/product-specs.html"
-        };
-     });
-    
-	app.directive("productTabs", function() {
-		return {
-			restrict : 'E',
-			templateUrl : "templates/product-tabs.html",
-			controller : function() {
-				this.tab = 1;			
-				this.isSet = function(checkTab) {
-					return this.tab === checkTab;
-				};			
-				this.setTab = function(setTab) {
-					this.tab = setTab;
-				};
-			},
-			controllerAs : 'panel'
-		};
-	});
-	
-	app.directive("productGallery", function() {
-		return {
-			restrict : 'E',
-			templateUrl : "templates/product-gallery.html",
-			controller : function() {
-				this.current = 0;
-		        this.setCurrent = function (val) {
-		            this.current = val || 0;
-		        };
-			},
-			controllerAs : 'gallery'
-		};
-	});
 })();
